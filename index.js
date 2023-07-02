585b3b1f883afae4306438588ebeaa8fbbdceab3
Abdullah Soomro <abdullahsoomro.official.com>
2023-07-03 03:03:30 +0500

    qwertyuiop

---
+++ index.js
@@ -0,0 +1,7 @@
+// const selectCategories = document.getElementById("select_categories");
+
+// selectCategories.addEventListener("change", function (e) {
+//   console.log(this.options[this.options.selectedIndex]);
+//   const option = this.options[this.options.selectedIndex];
+//   console.log(option.style);
+// });
